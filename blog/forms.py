56c1c8b0b74b3591526fbaada3b10a3b906c9ec9
Leo Miranda <leonardomir@gmail.com>
2023-03-17 15:03:18 -0400

    feat(template): share a post

--- blog/forms.py
+++ blog/forms.py
@@ -4,5 +4,5 @@ class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
-    coments = forms.CharField(required=False,
+    comments = forms.CharField(required=False,
                               widget=forms.Textarea)
