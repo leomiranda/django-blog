8c3c4c72dd9c303bea4c50beccc7f7cca34e377d
Leo Miranda <leonardomir@gmail.com>
2023-03-22 18:11:38 -0400

    feat: similar posts

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -26,13 +27,20 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    post_tags_id = post.tags.values_list('id', flat=True)
+    
+    similar_posts = Post.objects_published.filter(tags__in=post_tags_id)\
+                                          .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags', '-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 def post_list(request, tag_slug=None):
     object_list = Post.objects_published.all()
