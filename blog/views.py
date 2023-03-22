e3de66b821ec2873c6426d6c417c6d5539ea4527
Leo Miranda <leonardomir@gmail.com>
2023-03-21 21:34:54 -0400

    feat: comments and tags

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
-
-def post_list(request):
-    posts = Post.objects_published.all()
-    return render(request,
-                  'blog/post/list.html',
-                  {'posts': posts})
+from taggit.models import Tag
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
@@ -17,12 +12,36 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.objects_published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
 
@@ -32,9 +51,12 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
-                  {'page': page, 'posts': posts})
+                  {'page': page, 
+                   'posts': posts,
+                   'tag': tag})
 
 class PostListView(ListView):
     queryset = Post.objects_published.all()
